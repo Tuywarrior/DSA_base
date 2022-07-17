71dec69198577edba56b15cf9557188cbe06e4ef
Tuywarrior <87844490+Tuywarrior@users.noreply.github.com>
2022-07-17 19:43:37 +0530

    Add files via upload

---
+++ DSA base/sorting Algos/bubblesort.cpp
@@ -0,0 +1,20 @@
+//best case:O(N)
+//average case&& worst case:O(N)
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+int main(){
+  int n=10;
+  int arr[10]={6,3,56,78,1,0,-87,9,67,10};
+  for(int i=0;i<10;i++){
+    int flag=0;
+    for(int j=0;j<n-1;j++){
+      if(arr[j]>arr[j+1]){swap(arr[j],arr[j+1]);flag=1;}
+    }
+    n--;
+    if(flag==0)break;
+  }
+  for(int i=0;i<10;i++)cout<<arr[i]<<" ";
+}
